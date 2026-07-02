60de4db7e4ac148a2218f8976af83126e6f44c4c
Abhishek Mathur <abhishekmathur200624@gmail.com>
2026-07-02 12:43:36 +0530

    07-pattern

---
+++ Basic/patterns/07-pattern.py
@@ -0,0 +1,12 @@
+n = 5
+
+for i in range(1, n + 1):
+    # Print spaces
+    for j in range(n - i):
+        print(" ", end="")
+
+    # Print stars
+    for j in range(2 * i - 1):
+        print("*", end="")
+
+    print()
